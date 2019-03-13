c4062bb5956aba8a8a1cd602c72c8f8587e9b9be
Luke Burden <lukeburden@gmail.com>
2019-03-12 19:07:00 -0700

    Add FCM override for APNS sending

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -1,11 +1,20 @@
 from __future__ import unicode_literals
 
+import json
+import requests
+
+from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+from pyfcm import FCMNotification
+
 from .fields import HexIntegerField
 from .settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
+
+
+conversion_url = "https://iid.googleapis.com/iid/v1:batchImport"
 
 
 @python_2_unicode_compatible
@@ -62,7 +71,7 @@ class GCMDevice(Device):
 
 	class Meta:
 		verbose_name = _("GCM device")
-                unique_together = (('user', 'registration_id'),)
+		unique_together = (('user', 'registration_id'),)
 
 	def send_message(self, message, **kwargs):
 		from .gcm import gcm_send_message
@@ -85,6 +94,83 @@ class APNSDeviceQuerySet(models.query.QuerySet):
 			return apns_send_bulk_message(registration_ids=reg_ids, alert=message, **kwargs)
 
 
+def resolve_fcm_token(device):
+	"""
+	Use a Google API to convert an APNS token to one we can use with Firebase.
+	"""
+
+	if device.fcm_token:
+		# already set, nothing to do
+		return
+
+	headers = {
+		'Authorization': 'key={}'.format(SETTINGS["GCM_API_KEY"]),
+		'Content-type': 'application/json'
+	}
+
+	body = {
+		# hardcode this for now
+		"application": "com.hitchplanet.app",
+		"sandbox": True if 'sandbox' in (SETTINGS["APNS_HOST"] or '') else False,
+		"apns_tokens": [
+			device.registration_id
+		]
+	}
+
+	response = requests.post(
+		conversion_url, headers=headers, data=json.dumps(body)
+	)
+
+	fcm_token = None
+	for r in response.json()["results"]:
+		if r["apns_token"] == device.registration_id and r["status"] == "OK":
+			fcm_token = r["registration_token"]
+			break
+
+	if fcm_token:
+		device.fcm_token = fcm_token
+		device.save(update_fields=('fcm_token',))
+
+
+configuration_errors = set(
+	['MismatchSenderId']
+)
+
+unrecoverable_errors = set(
+	['MissingRegistration', 'InvalidRegistration', 'NotRegistered']
+)
+
+
+def deactivate_device_on_fcm_error(device, result):
+	"""
+	Deactive a device if it fails to be sent a message due to an unrecoverable issue.
+
+	An example error:
+
+		{
+			'multicast_ids': [4926029746364504051],
+			'success': 0,
+			'failure': 1,
+			'canonical_ids': 0,
+			'results': [
+				{'error': 'InvalidRegistration'}
+			],
+			'topic_message_id': None
+		}
+	"""
+	if result['failure'] > 0:
+		if result['results'][0]['error'] in unrecoverable_errors:
+			device.active = False
+			device.save(update_fields=('active',))
+		elif result['results'][0]['error'] in configuration_errors:
+			raise ImproperlyConfigured(
+				'FCM config issue when sending to device {}: {}'.format(
+					device.id,
+					result['results'][0]['error']
+				)
+			)
+
+
 class APNSDevice(Device):
 	device_id = models.UUIDField(
 		verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
@@ -93,17 +179,35 @@ class APNSDevice(Device):
 	registration_id = models.CharField(
 		verbose_name=_("Registration ID"), max_length=200, blank=False
 	)
+	# we create an FCM token for this device and send via FCM rather than
+	# terrible APNS
+	fcm_token = models.TextField(null=True)
 
 	objects = APNSDeviceManager()
 
 	class Meta:
 		verbose_name = _("APNS device")
-                unique_together = (('user', 'registration_id'),)
+		unique_together = (('user', 'registration_id'),)
 
-	def send_message(self, message, **kwargs):
-		from .apns import apns_send_message
+	def send_message(self, message, sound=None, category=None, extra=None, **kwargs):
 
-		return apns_send_message(registration_id=self.registration_id, alert=message, **kwargs)
+		use_fcm = SETTINGS.get("APNS_USE_FCM", False)
+		if use_fcm and not self.fcm_token:
+			resolve_fcm_token(self)
+
+		if use_fcm and self.fcm_token:
+			# great, let's use it!
+			push_service = FCMNotification(api_key=SETTINGS["GCM_API_KEY"])
+			result = push_service.notify_single_device(
+				registration_id=self.fcm_token,
+				message_title=message,
+				message_body=message,
+				data_message=extra
+			)
+			deactivate_device_on_fcm_error(self, result)
+		else:
+			from .apns import apns_send_message
+			return apns_send_message(registration_id=self.registration_id, alert=message, **kwargs)
 
 
 class WNSDeviceManager(models.Manager):
